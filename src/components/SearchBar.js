e377a93d8cce32d5e95bbb06a890ae97801ffbba
IchwanSetyawan <ichwansetyawan2015@gmail.com>
2021-03-03 17:11:23 +0700

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <span><b>Search bar</b></span>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
+        )
+    }
+}
